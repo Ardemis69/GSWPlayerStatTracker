48879d6f22e9d335b84f0429bf1518b70c98f90c
Pranjul Singh <68863372+Ardemis69@users.noreply.github.com>
2022-11-17 20:27:27 -0500

    added user roles

--- GSWPlayerStatTracker/Program.cs
+++ GSWPlayerStatTracker/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    //enabling roles on the new user class
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
